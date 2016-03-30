f4e26bf4d67b895eca7712da62f87d1dd140606b
bbugh <brainbag@gmail.com>
2016-03-30 06:26:17 -0500

    Rebuilt index.js

--- build/index.js
+++ build/index.js
@@ -74,12 +74,6 @@ var SearchBar = React.createClass({
   handleQueryTextChange: function (e) {
     this.setState({ textQuery: e.target.value });
   },
-  handleSearch: function (e) {
-    // e.preventDefault();
-    var textQuery = this.state.textQuery.trim();
-    var goalsQuery = this.state.goalsQuery;
-    this.props.onSearch({ textQuery: textQuery, goalsQuery: goalsQuery });
-  },
   handleSubmit: function (e) {
     e.preventDefault();
     var textQuery = this.state.textQuery.trim();
@@ -92,20 +86,16 @@ var SearchBar = React.createClass({
   render: function () {
     return React.createElement(
       'div',
-      { id: 'search_bar ui segment' },
+      { id: 'search_bar' },
       React.createElement(
-        'div',
-        { className: 'ui action input' },
+        'form',
+        { onSubmit: this.handleSubmit },
         React.createElement('input', { type: 'text',
           placeholder: 'Enter search text...',
           value: this.state.textQuery,
           onChange: this.handleQueryTextChange
         }),
-        React.createElement(
-          'button',
-          { onClick: this.handleSearch, className: 'ui button' },
-          'Search'
-        )
+        React.createElement('input', { type: 'submit' })
       ),
       React.createElement(GoalList, { goals: this.props.goals, onGoalsChanged: this.handleGoalsChange })
     );
@@ -118,66 +108,45 @@ var ProjectRow = React.createClass({
   render: function () {
     return React.createElement(
       'div',
-      { className: 'item' },
+      { className: 'entry' },
       React.createElement(
-        'div',
-        { className: 'ui small image' },
-        React.createElement('img', { src: this.props.project.img_source })
+        'h1',
+        null,
+        this.props.project.title
       ),
       React.createElement(
         'div',
-        { className: 'content' },
+        { className: 'entry_description' },
+        React.createElement('img', { className: 'entry_img', src: this.props.project.img_source }),
         React.createElement(
-          'div',
-          { className: 'header' },
-          this.props.project.title
+          'p',
+          null,
+          this.props.project.description
         ),
         React.createElement(
-          'div',
-          { className: 'meta' },
+          'p',
+          null,
           React.createElement(
-            'span',
-            { className: 'project_link' },
-            React.createElement(
-              'a',
-              { href: this.props.project.demo_url },
-              'Learn More'
-            )
+            'a',
+            { href: this.props.project.demo_url },
+            'Learn More'
           ),
-          '|',
+          ' | ',
           React.createElement(
-            'span',
-            { className: 'github_link' },
-            React.createElement(
-              'a',
-              { href: this.props.project.demo_url },
-              'See code on Github'
-            )
+            'a',
+            { href: this.props.project.repo_url },
+            'GitHub Source'
           )
         ),
         React.createElement(
-          'div',
-          { className: 'description' },
+          'p',
+          null,
           React.createElement(
-            'p',
+            'strong',
             null,
-            this.props.project.description
-          )
-        ),
-        React.createElement(
-          'div',
-          { className: 'extra' },
-          React.createElement(
-            'div',
-            { className: 'ui label' },
-            'IMAX'
+            'Goals: '
           ),
-          React.createElement(
-            'div',
-            { className: 'ui label' },
-            React.createElement('i', { className: 'globe icon' }),
-            ' Additional Languages'
-          )
+          this.props.project.tags
         )
       )
     );
@@ -201,12 +170,8 @@ var ProjectList = React.createClass({
     }.bind(this));
     return React.createElement(
       'div',
-      { className: 'ui segment loading' },
-      React.createElement(
-        'div',
-        { id: 'projectList', className: 'ui items' },
-        projectNodes
-      )
+      { id: 'projectList' },
+      projectNodes
     );
   }
 });
@@ -215,7 +180,7 @@ var OpenSourceGoalsTable = React.createClass({
   displayName: 'OpenSourceGoalsTable',
 
   getInitialState: function () {
-    return { projects: this.props.projects };
+    return { projects: [] };
   },
   onAjaxComplete: function (data, two, three) {
     this.setState({ projects: data.params.entries });
@@ -233,7 +198,7 @@ var OpenSourceGoalsTable = React.createClass({
   render: function () {
     return React.createElement(
       'div',
-      { className: 'segments' },
+      null,
       React.createElement(SearchBar, { onSearch: this.handleSearch, goals: this.props.goals }),
       React.createElement(ProjectList, { projects: this.state.projects })
     );
@@ -242,20 +207,6 @@ var OpenSourceGoalsTable = React.createClass({
 
 var sustainableGoals = [{ id: 1, image: "sdg-icon-01", goal: "1. No Poverty" }, { id: 2, image: "sdg-icon-02", goal: "2. Zero Hunger" }, { id: 3, image: "sdg-icon-03", goal: "3. Good Health and Well-Being" }, { id: 4, image: "sdg-icon-04", goal: "4. Quality Education" }, { id: 5, image: "sdg-icon-05", goal: "5. Gender Equality" }, { id: 6, image: "sdg-icon-06", goal: "6. Clean Water and Sanitation" }, { id: 7, image: "sdg-icon-07", goal: "7. Affordable and Clean Energy" }, { id: 8, image: "sdg-icon-08", goal: "8. Decent Work and Economic Growth" }, { id: 9, image: "sdg-icon-09", goal: "9. Industry, Innovation and Infrastructure" }, { id: 10, image: "sdg-icon-10", goal: "10. Reduced Inequalities" }, { id: 11, image: "sdg-icon-11", goal: "11. Sustainable Cities and Communities" }, { id: 12, image: "sdg-icon-12", goal: "12. Responsible Consumption and Production" }, { id: 13, image: "sdg-icon-13", goal: "13. Climate Action" }, { id: 14, image: "sdg-icon-14", goal: "14. Life Below Water" }, { id: 15, image: "sdg-icon-15", goal: "15. Life on Land" }, { id: 16, image: "sdg-icon-16", goal: "16. Peace, Justice and Strong Institutions" }, { id: 17, image: "sdg-icon-17", goal: "17. Partnerships for the Goals" }];
 
-var projects = [{
-  "Project Name": "Example Project",
-  "Description": "Project Drawdown is a research and communications organization that aims to describe when and how humanity can reach climate drawdown - the point at which greenhouse gas concentrations in the atmosphere begin to decline on a year-to-year basis. We are conducting a global analysis of 100 climate solutions and their potential financial and climate impact over the next 30 years and making this information widely available through an internationally published book, online interactive platform and open-source database.",
-  "Image URL": "http://www.kobotoolbox.org/sites/toolbox/files/images/features/kc_map.png",
-  "Related SDGs": "9. Industry, Innovation and Infrastructure",
-  "Demo or Website URL": "https://www.google.com",
-  "Code Repository URL": "https://www.github.com"
-}, {
-  "Project Name": "Super Project",
-  "Description": "Project Drawdown is a research and communications organization that aims to describe when and how humanity can reach climate drawdown - the point at which greenhouse gas concentrations in the atmosphere begin to decline on a year-to-year basis. We are conducting a global analysis of 100 climate solutions and their potential financial and climate impact over the next 30 years and making this information widely available through an internationally published book, online interactive platform and open-source database.",
-  "Image URL": "http://www.kobotoolbox.org/sites/toolbox/files/images/features/kc_map.png",
-  "Related SDGs": "9. Industry, Innovation and Infrastructure",
-  "Demo or Website URL": "https://www.google.com",
-  "Code Repository URL": "https://www.github.com"
-}];
+var projects = [{}];
 
 ReactDOM.render(React.createElement(OpenSourceGoalsTable, { projects: projects, goals: sustainableGoals }), document.getElementById("container"));
